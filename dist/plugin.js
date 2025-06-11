66bffc003683abfb045064ea9a264ca14ec45179
Massimo Melina <a@rejetto.com>
2025-06-11 17:25:29 +0200

    wrong repo

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,5 +1,5 @@
-exports.version = 1
+exports.version = 1.01
 exports.description = "Expand file-show to support MarkDown files"
 exports.apiRequired = 8.22
 exports.frontend_js = ["main.js", "marked.min.js"]
-exports.repo = "rejetto/show-md"
+exports.repo = "rejetto/hfs-show-md"
